a44d273d7815dcf1fbd36ce5ec538ebc96aea9ee
ManuRamos <ManuRamos@MANU>
2023-09-29 03:51:21 -0600

    Agregar archivos de proyecto.

---
+++ Actividad2Unidad2/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Actividad2Unidad2.Models;
+
+namespace Actividad2Unidad2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(ConvertidorViewModels con, string moneda)
+        {
+            if (moneda == "Peso")
+            {
+                con.Resultado = con.Dinero * 18;
+            }
+            else if(moneda == "Dolar")
+            {
+                con.Resultado += con.Dinero / 18;
+            }
+            return View(con);
+        }
+    }
+}
---
+++ Actividad2Unidad2/Models/ConvertidorViewModels.cs
@@ -0,0 +1,8 @@
+﻿namespace Actividad2Unidad2.Models
+{
+    public class ConvertidorViewModels
+    {
+        public Decimal Dinero { get; set; }
+        public Decimal Resultado{ get;set; }
+    }
+}
